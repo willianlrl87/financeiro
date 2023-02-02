4f35789b508c1c10b99e114e5fd57b5d3d141263
Willian Luiz <willianlrl85@gmail.com>
2023-02-02 10:40:19 -0300

    Problem with map

--- src/Components/OperationRegister.jsx
+++ src/Components/OperationRegister.jsx
@@ -1,17 +1,23 @@
 import * as styles from './styles';
 import { FaTrash, FaArrowCircleUp, FaArrowCircleDown } from 'react-icons/fa';
+import OperationsContext from '../contexts/OperationsContext';
+import {useState} from 'react';
 
 export const OperationRegister = () => {
-    const type='entrada';
+    const type='saida';
+    const {operationsRegistered, setOperationsRegistered} = useState(OperationsContext);
     return (
-        <styles.OperationRegisterDiv>
-            <span style={{flex:'2'}}>description ashdjahdkj2hadkahdkahdskjahsdkrj</span>
-            <span style={{flex:'2'}}>value</span>
-            <div style={{display:'flex', flex: '1', justifyContent:'space-between'}}>
-                <span>{type == 'entrada' ? <FaArrowCircleUp color='green'/> 
-                    : <FaArrowCircleDown color='red' /> }</span>
-                <span><FaTrash /></span>
-            </div>
-        </styles.OperationRegisterDiv>
+            {operationsRegistered.map((operation) => {
+                <styles.OperationRegisterDiv>
+                    <span style={{flex:'2'}}>{operation.description}</span>
+                    <span style={{flex:'2'}}>{operation.value}</span>
+                    <div style={{display:'flex', flex: '1', justifyContent:'space-between'}}>
+                        <span>{operation.type == 'entrada' ? <FaArrowCircleUp color='green'/> 
+                            : <FaArrowCircleDown color='red' /> }</span>
+                        <span><FaTrash /></span>
+                    </div>
+                </styles.OperationRegisterDiv>
+            })
+        }
     )
 }
