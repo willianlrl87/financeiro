b96142d57b2dd19a8d13c11b989cf8eac0709900
Willian Luiz <willianlrl85@gmail.com>
2023-02-09 14:50:12 -0300

    Revert "LocalStorage concluído"
    This reverts commit 3db1ad3edf714b77003c0c2669564a0cb1591b4c.

--- src/hooks/useRecoveryFromLocalStorage.js
+++ src/hooks/useRecoveryFromLocalStorage.js
@@ -5,8 +5,15 @@ export const useRecoveryFromLocalStorage = (operationsRegistered) => {
     if(localStorage.hasOwnProperty('operações')){
         localData = JSON.parse(localStorage.getItem('operações'));    
         // pesquisar como passar o setState como parametro de função
+<<<<<<< HEAD
 
     }
 
+=======
+
+    }
+    console.log(localData);
+
+>>>>>>> parent of 3db1ad3 (LocalStorage concluído)
     return null;
 }
