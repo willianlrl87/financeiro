20f885b6f2944e93c6342d0a9969d4d341f254e9
Willian Luiz <willianlrl85@gmail.com>
2023-02-09 14:45:53 -0300

    t

--- src/hooks/useFormatInputAsNumber.js
+++ src/hooks/useFormatInputAsNumber.js
@@ -1,9 +1,13 @@
 export const useFormatInputAsNumber = (value) => {
-    console.log(`Parametro ${value}`);
-    const valueString = toString(value);
-    console.log(`string ${valueString}`);
-    console.log(valueString);
-    const formattedNumber = Number(value.replace(/\D/g,''));
-    console.log(`useFormatInputAsNumber ${formattedNumber}`);
-    return Number(formattedNumber);
+    // console.log(`Parametro ${value}`);
+    // const valueString = toString(value);
+    // console.log(`string ${valueString}`);
+    // console.log(valueString);
+    // const formattedNumber = Number(value.replace(/\D/g,''));
+    // console.log(`useFormatInputAsNumber ${formattedNumber}`);
+    // return Number(formattedNumber);
+    
+        const formattedNumber = Number(value.replace(/\D/g,''));
+        return formattedNumber;
+    
 }
